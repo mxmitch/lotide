a158f08b69036fc54f7e131ff18ab5306d8aa31c
Mitch Lum <mklum88@gmail.com>
2019-11-27 03:40:05 +0000

    fixed bug issue in for loop

--- eqArrays.js
+++ eqArrays.js
@@ -1,18 +1,14 @@
 let eqArrays = function (array1, array2) {
-  if (array1.length === array2.length) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
     for (let i = 0; i < array1.length; i++) {
-      if (typeof array1[i] !== typeof array2[i]) {
-        return false;
+      if (array1[i] !== array2[i]) {
+        return false
       }
     }
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] === array2[i]) {
-        return true;
-      }
-      return false;
-    }
+    return true;
   }
-  return false;
 };
 
 const assertEqual = function (actual, expected) {
@@ -24,4 +20,4 @@ const assertEqual = function (actual, expected) {
 };
 
 // TEST CODE
-assertEqual(eqArrays([1, "2", 3], [1, 2, 3]), true);
+console.log(eqArrays([1, 2, 3], [1, 2, 3]));
