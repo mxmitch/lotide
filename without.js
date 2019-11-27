6ec179688bad69490c25481bd3a160f280ee0bf2
Mitch Lum <mklum88@gmail.com>
2019-11-27 20:24:50 +0000

    function to remove values of one array from another

---
+++ without.js
@@ -0,0 +1,39 @@
+//FUNCTIONS
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (typeof array1[i] !== typeof array2[i]) {
+        return false;
+      }
+    }
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const without = function(array1, array2) {
+  let newArray = [];
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      newArray.push(array1[i]);
+    }
+  }
+  return newArray;
+};
+
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
