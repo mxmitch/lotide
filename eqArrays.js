766468caff7549992ea16ba01183e6e94aab6a62
Mitch Lum <mklum88@gmail.com>
2019-11-27 03:45:22 +0000

    added condition to check type of array contents

--- eqArrays.js
+++ eqArrays.js
@@ -3,6 +3,11 @@ let eqArrays = function (array1, array2) {
     return false;
   } else {
     for (let i = 0; i < array1.length; i++) {
+      if (typeof array1[i] !== typeof array2[i]) {
+        return false
+      }
+    }
+    for (let i = 0; i < array1.length; i++) {
       if (array1[i] !== array2[i]) {
         return false
       }
@@ -20,4 +25,4 @@ const assertEqual = function (actual, expected) {
 };
 
 // TEST CODE
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), true); // => should PASS
