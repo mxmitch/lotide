ee475eeefabe76c7dfc5fbd635dc0e8c983be68a
Mitch Lum <mklum88@gmail.com>
2019-11-28 01:50:35 +0000

    function to find the object's key by a given value

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj, value) {
+  let result = '';
+  for (const property in obj) {
+    if (obj[property] === value) {
+      result = property;
+    }
+  }
+  if (result === '') {
+    return undefined;
+  }
+  return result;
+};
+
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
