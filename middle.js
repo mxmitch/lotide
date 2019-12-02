873d3b4090ee9a6661ddc71a0760d7aed0d42819
Mitch Lum <mklum88@gmail.com>
2019-12-02 22:09:58 +0000

    add refactored middle function with modules

--- middle.js
+++ middle.js
@@ -1,31 +1,4 @@
-//FUNCTIONS
-const eqArrays = function (array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (typeof array1[i] !== typeof array2[i]) {
-        return false
-      }
-    }
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false
-      }
-    }
-    return true;
-  }
-};
-
-const assertArraysEqual = function (actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const middle = function (array) {
+const middle = function(array) {
   let newArray = [];
   if (array.length >= 3) {
     if (array.length % 2 !== 0) {
@@ -40,9 +13,4 @@ const middle = function (array) {
   return newArray;
 }
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
